cd60ee46173b630dcbb762d20f5193aa97bfed4a
aldevops <aldevops@outlook.com>
2019-04-04 18:43:40 +0530

    install java apache tomcat using playbook

---
+++ dum/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Installation of JAVA8
+sudo apt-get update -y
+sudo apt-get install openjdk-8-jdk-headless -y
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+#sudo apt-get update -y
+#sudo apt-get install tomcat8 -y
+#systemctl start tomcat8
+#systemctl status tomcat8
